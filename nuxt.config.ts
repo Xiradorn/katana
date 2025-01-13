e982ce14bd76cafe7f45c9f11c9790956533c40f
Xiradorn <xiradorn@gmail.com>
2025-01-14 00:27:00 +0100

    chore: Versione aggiornata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				future: {
-								compatibilityVersion: 4
-				},
-				compatibilityDate: '2024-11-01',
-				devtools: { enabled: true },
+	future: {
+		compatibilityVersion: 4
+	},
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	ssr: false,
 
-				modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image'],
+	modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image'],
 
-				imports: {
-								dirs: ['composables/']
-				},
+	imports: {
+		dirs: ['composables/']
+	},
 
-				components: [
-								{
-												pathPrefix: false,
-												path: '~/components'
-								}
-				],
+	components: [
+		{
+			pathPrefix: false,
+			path: '~/components'
+		}
+	],
 
-				css: ['~/assets/css/main.css']
+	css: ['~/assets/css/main.css']
 })
